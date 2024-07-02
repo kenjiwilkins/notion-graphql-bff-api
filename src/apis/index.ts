3187fd8ccfb9fcd22c368d6005d521d4f74b29a2
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-07-02 21:43:34 +0900

    add recipe tag api

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -25,3 +25,38 @@ export async function getBooks(config: { start?: string; amount?: number }) {
     return Promise.reject(error);
   }
 }
+
+export async function getBook(id: string) {
+  try {
+    const notion = getNotionClient();
+    const response = await notion.pages.retrieve({
+      page_id: id,
+    });
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export async function getRecipeTags(config: {
+  start?: string;
+  amount?: number;
+}) {
+  try {
+    const notion = getNotionClient();
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_RECIPE_TAG_ID || "",
+      page_size: config.amount || 10,
+      start_cursor: config.start ? config.start : undefined,
+      // sorts: [
+      //   {
+      //     property: "updated_at",
+      //     direction: "descending",
+      //   },
+      // ],
+    });
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
